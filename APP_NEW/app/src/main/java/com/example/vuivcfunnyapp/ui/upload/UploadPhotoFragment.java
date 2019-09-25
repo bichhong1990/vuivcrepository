3152501d9e3e010d5a56bfe6de3ded1f0c236d21
Trần Thị Bích Hồng <tranthibichhong90proweb@gmail.com>
2019-09-25 13:49:45 +0700

    Temp

--- APP_NEW/app/src/main/java/com/example/vuivcfunnyapp/ui/upload/UploadPhotoFragment.java
+++ APP_NEW/app/src/main/java/com/example/vuivcfunnyapp/ui/upload/UploadPhotoFragment.java
@@ -174,13 +174,13 @@ public class UploadPhotoFragment extends Fragment {
         }
 // Tell the media scanner about the new file so that it is
 // immediately available to the user.
-//        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
-//                new MediaScannerConnection.OnScanCompletedListener() {
-//                    public void onScanCompleted(String path, Uri uri) {
-//                        Log.i("ExternalStorage", "Scanned " + path + ":");
-//                        Log.i("ExternalStorage", "-> uri=" + uri);
-//                    }
-//                });
+        MediaScannerConnection.scanFile(getContext(), new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
     }
 
 
